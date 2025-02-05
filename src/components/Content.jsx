431032862465687a38c8f19dc4bae0f6b569f3eb
Haley Goubert <hmgoubert@gmail.com>
2025-02-04 19:33:28 -0500

    Initial commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,16 @@
+import "react";
+
+
+const Content = () => {
+  return (
+    <div className="content">
+      <p>Cinderella&apos;s father died and left her with her evil stepmother and stepsisters.</p>
+      <p>The night of the ball Cinderella recieved a fairy godmother who used magic to make her a beautiful dress for the royal ball.</p>
+      <p>That night she met the prince Henry and fell in love.</p>
+      <p>When the clock struck midnight, the magic wore off so she ran home but lost a glass slipper on the way.</p>
+      <p>Prince Henry tried the shoe on every woman in town, and finally found Cinderella and they lived happily ever after.</p>
+    </div>
+  );
+};
+
+export default Content;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+const Footer = () => {
+  return <footer className="footer"><p>Moral: If you stay true to yourself and never let anyone get you down, 
+    you will always prosper in the end.</p></footer>;
+};
+
+export default Footer;
